02bed378f81b809f0027df566193277c248d15f4
DrStrange1224 <maksim.petr0w@mail.ru>
2026-06-19 23:24:03 +0400

    feat: set editor config

--- scripts/controllers/main_controller.cs
+++ scripts/controllers/main_controller.cs
@@ -3,17 +3,19 @@ using Godot;
 public partial class main_controller : Node
 {
     private const string LOG_PATH = "D:/godot_projects/new-game-project/logs"; //TODO clear hardcode
-    private const string LOG_NAME = "log.txt";
+    private const string LOG_NAME = "log.log";
     private const bool DEBUG_MODE = true;
     private Logger logger;
 
     public override void _Ready()
     {
+        setupLogger();
+        logger.Log("Init log");
         GD.Print("Hello");
     }
 
     public void setupLogger()
     {
-        logger = new Logger(Logger.Config.GetInstance(LOG_NAME, LOG_PATH));
+        //logger = new Logger(Logger.Config.GetInstance(LOG_NAME, LOG_PATH));
     }
 }
