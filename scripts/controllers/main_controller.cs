d5a3e5b3e9263774292a3c75cc253deb1a96dc40
DrStrange1224 <maksim.petr0w@mail.ru>
2026-06-20 00:22:35 +0400

    fix: fixed linter

--- scripts/controllers/main_controller.cs
+++ scripts/controllers/main_controller.cs
@@ -1,21 +1,17 @@
 using Godot;
 
-public partial class main_controller : Node
-{
+public partial class main_controller : Node {
     private const string LOG_PATH = "D:/godot_projects/new-game-project/logs"; //TODO clear hardcode
     private const string LOG_NAME = "log.log";
-    private const bool DEBUG_MODE = true;
     private Logger logger;
 
-    public override void _Ready()
-    {
+    public override void _Ready() {
         setupLogger();
         logger.Log("Init log");
         GD.Print("Hello");
     }
 
-    public void setupLogger()
-    {
-        //logger = new Logger(Logger.Config.GetInstance(LOG_NAME, LOG_PATH));
+    public void setupLogger() {
+        logger = new Logger();
     }
 }
--- scripts/utils/logger.cs
+++ scripts/utils/logger.cs
@@ -1,35 +1,36 @@
 using System;
 using System.IO;
 
-public class Logger
-{
-    public struct Config
-    {
-        private string _filePath;
-        private string _fileName;
-        private bool _isRollback;
-        private int _rollbackCount;
+public class Logger {
+    public struct Config {
+        public string filePath = "";
+        public string fileName = "";
+        public bool isRollback = false;
+        public int rollbackCount = 0;
 
-        public string FilePath
-        {
-            get { }
-            set { }
+        public Config(string filePath, string fileName, int rollbackCount) {
+            this.filePath = filePath;
+            this.fileName = fileName;
+            if (rollbackCount <= 0) {
+                rollbackCount = 0;
+                isRollback = false;
+            }
+            else {
+                isRollback = true;
+            }
         }
     }
 
     private Config _config;
 
-    public Logger(Config config)
-    {
+    public Logger(Config config) {
         ThisConfig = config;
         loadConfig();
     }
 
-    public Config ThisConfig
-    {
+    public Config ThisConfig {
         get { return _config; }
-        set
-        {
+        set {
             //TODO checks
             _config = value;
         }
