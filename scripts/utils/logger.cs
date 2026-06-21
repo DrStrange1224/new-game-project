cd9634bc4b1963a2d2b8018d105c1cd5abb40c2c
DrStrange1224 <maksim.petr0w@mail.ru>
2026-06-21 15:35:21 +0400

    refactor: changed logger config type from struct to record struct

--- scripts/utils/logger.cs
+++ scripts/utils/logger.cs
@@ -3,18 +3,9 @@ using Godot;
 using Godot.Collections;
 
 public class Logger {
-    public readonly struct Config {
-        public readonly string filePath;
-        public readonly string fileName;
-
-        public Config(string filePath, string fileName) {
-            this.filePath = filePath;
-            this.fileName = fileName;
-        }
-    }
-
+    public readonly record struct Config(string FilePath, string FileName);
     public readonly Config config;
-    private StreamWriter _streamWriter;
+    private readonly StreamWriter _streamWriter;
 
     public Logger(Dictionary configDict) {
         GD.Print(configDict.Keys);
@@ -22,7 +13,7 @@ public class Logger {
             configDict["path"].ToString(),
             $"{configDict["fileName"]}.{configDict["fileExtension"]}"
         );
-        _streamWriter = new StreamWriter($"{config.filePath}\\{config.fileName}");
+        _streamWriter = new StreamWriter($"{config.FilePath}\\{config.FileName}");
     }
 
     ~Logger() {
